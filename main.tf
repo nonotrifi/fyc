d7ff16e94017a051c5a58056400dbd7b8727238b
ntrifi <trifinourdine@outlook.fr>
2024-01-18 18:07:30 +0100

    adding for each & vpc

--- main.tf
+++ main.tf
@@ -3,28 +3,37 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-  ami           = "ami-0005e0cfe09cc9050"
+  ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
   key_name      = "dpp"
-  security_groups = [ "demo-sg" ]
+  #   security_groups = ["demo-sg"] 
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  subnet_id              = aws_subnet.dpw-public_subent_01.id
+
+
+  for_each = toset(["Jenkins-master", "build-slave", "ansible"])
+   tags = {
+     Name = "${each.key}"
+   }
 }
 
 
 resource "aws_security_group" "demo-sg" {
-  name = "demo-sg"
+  name        = "demo-sg"
   description = "SSH ACCESS"
+  vpc_id      = aws_vpc.dpw-vpc.id
 
-    ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
